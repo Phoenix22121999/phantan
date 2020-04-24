71208136366feec2a47a1d84af4e30796c08b063
NolanNgo <48820527+NolanNgo@users.noreply.github.com>
2020-04-24 22:29:47 +0700

    24/4/2020_10:29pm

--- php/conn2.php
+++ php/conn2.php
@@ -1,13 +1,2 @@
 <?php
-$serverName = "25.89.202.22,1433"; //serverName\instanceName, portNumber (default is 1433)
-$connectionInfo = array("Database" => "QLTK", "UID" => "sa", "PWD" => "123456");
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
-if (!$conn) {
-    $serverName = "DESKTOP-1DPN8B8\ACCOUNT_SERVER2"; //serverName\instanceName, portNumber (default is 1433)
-    $connectionInfo = array("Database" => "QLTK", "UID" => "sa", "PWD" => "123456");
-    $conn = sqlsrv_connect($serverName, $connectionInfo);
-    if (!$conn) {
-        exit('Kết nối không thành công!');
-    }
-}
+require('connCN1.php');
